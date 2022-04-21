2907fd3e4cc7a136f91cd976b18550cc528102a4
handwoong <handwoong@gmail.com>
2022-04-22 00:51:13 +0900

    [FE][#24] test: 회원가입폼 mui 테스트 코드 수정

--- front/src/__tests__/pages/User/Auth/Login.test.js
+++ front/src/__tests__/pages/User/Auth/Login.test.js
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Login from "../../../../pages/User/Auth/Login";
 import userEvent from "@testing-library/user-event";
 import { QueryClientProvider, QueryClient } from "react-query";
--- front/src/__tests__/pages/User/Auth/Register.test.js
+++ front/src/__tests__/pages/User/Auth/Register.test.js
@@ -5,38 +5,42 @@ import userEvent from "@testing-library/user-event";
 describe("Check the form required for register", () => {
   it("Check service Logo", () => {
     render(<Register />);
-    const serviceLogo = screen.getByRole("heading", { name: "문해한 하루" });
-    expect(serviceLogo).toBeInTheDocument();
+    const registerLogo = screen.getByRole("heading", { name: /sign up/i });
+    expect(registerLogo).toBeInTheDocument();
   });
 
   it("Check email form", () => {
     render(<Register />);
-    const emailForm = screen.getByLabelText("이메일");
+    const emailForm = screen.getByRole("textbox", { name: "Email Address" });
     expect(emailForm).toBeInTheDocument();
   });
 
   it("Check password form", () => {
     render(<Register />);
-    const passwordForm = screen.getByLabelText("비밀번호");
+    const passwordForm = screen.getByLabelText(/^Password/, {
+      exact: false,
+    });
     expect(passwordForm).toBeInTheDocument();
   });
 
   it("Check confirm password form", () => {
     render(<Register />);
-    const passwordForm = screen.getByLabelText("비밀번호 확인");
-    expect(passwordForm).toBeInTheDocument();
+    const confirmPasswordForm = screen.getByLabelText("Confirm Password", {
+      exact: false,
+    });
+    expect(confirmPasswordForm).toBeInTheDocument();
   });
 
   it("Check nickname form", () => {
     render(<Register />);
-    const nickNameForm = screen.getByLabelText("닉네임");
+    const nickNameForm = screen.getByRole("textbox", { name: "Nickname" });
     expect(nickNameForm).toBeInTheDocument();
   });
 
   it("Check submit button", () => {
     render(<Register />);
     const submitButton = screen.getByRole("button");
-    expect(submitButton).toHaveTextContent("회원가입");
+    expect(submitButton).toHaveTextContent(/sign up/i);
   });
 });
 
@@ -45,31 +49,27 @@ describe("Auth button's action", () => {
     const onSubmit = jest.fn();
     render(<Register onSubmit={onSubmit} />);
 
-    const submitButton = screen.getByRole("button", { name: "회원가입" });
+    const submitButton = screen.getByRole("button", { name: /sign up/i });
 
-    const emailForm = screen.getByLabelText("이메일");
-    userEvent.clear(emailForm);
+    const emailForm = document.querySelector("#register-email");
     userEvent.type(emailForm, "abcd@naver.com");
-    expect(submitButton).toBeDisabled();
+    expect(submitButton).toHaveClass("Mui-disabled");
 
-    const passwordForm = screen.getByLabelText("비밀번호");
-    userEvent.clear(passwordForm);
+    const passwordForm = document.querySelector("#register-password");
     userEvent.type(passwordForm, "test1234");
-    expect(submitButton).toBeDisabled();
+    expect(submitButton).toHaveClass("Mui-disabled");
 
-    const confirmPasswordForm = screen.getByLabelText("비밀번호 확인");
-    userEvent.clear(confirmPasswordForm);
+    const confirmPasswordForm = document.querySelector("#confirmPassword");
     userEvent.type(confirmPasswordForm, "test1234");
-    expect(submitButton).toBeDisabled();
+    expect(submitButton).toHaveClass("Mui-disabled");
 
-    const nicknameForm = screen.getByLabelText("닉네임");
-    userEvent.clear(nicknameForm);
+    const nicknameForm = document.querySelector("#nickName");
     userEvent.type(nicknameForm, "테스트닉네임");
-    expect(submitButton).toBeEnabled();
+    expect(submitButton).not.toHaveClass("Mui-disabled");
 
     userEvent.click(submitButton);
     expect(onSubmit).toBeCalled();
 
-    await screen.findByRole("button", { name: "회원가입" });
+    await screen.findByRole("button", { name: /sign up/i });
   });
 });
