d97122192769ac80a44c4493c0318d5f7ec7db36
Hye-S <gptjs7970@naver.com>
2022-04-21 22:54:13 +0900

    [BE] feat: models/User.js 작성

---
+++ back/src/db/models/User.js
@@ -0,0 +1,42 @@
+import { UserModel } from "../schemas/user";
+
+class User {
+  static async create({ newUser }) {
+    const createdNewUser = await UserModel.create(newUser);
+    return createdNewUser;
+  }
+
+  static async findByEmail({ email }) {
+    const user = await UserModel.findOne({ email });
+    return user;
+  }
+
+  static async findById({ userId }) {
+    const user = await UserModel.findOne({ _id: userId });
+    return user;
+  }
+
+  static async findAll() {
+    const users = await UserModel.find({});
+    return users;
+  }
+
+  static async update({ userId, toUpdate }) {
+    const filter = { _id: userId };
+    const option = { returnOriginal: false };
+
+    const updatedUser = await UserModel.findOneAndUpdate(
+      filter,
+      toUpdate,
+      option
+    );
+    return updatedUser;
+  }
+
+  static async delete({ userId }) {
+    const deletedUser = await UserModel.deleteOne({ _id: userId });
+    return deletedUser;
+  }
+}
+
+export { User };
