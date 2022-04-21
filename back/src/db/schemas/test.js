e8680ad08c494044d2b5b87f6cd31c4b0ba6e96d
Hye-S <gptjs7970@naver.com>
2022-04-21 22:32:02 +0900

    [BE] feat: test DB Schema 변경(content 추가)

--- back/src/db/schemas/test.js
+++ back/src/db/schemas/test.js
@@ -3,6 +3,7 @@ import { Schema, model } from "mongoose";
 const TestSchema = new Schema(
   {
     question: { type: String, required: true, },
+    content: { type: String, required: false, default: "", },
     choices: [{ type: String, required: true,}],
     answer: { type: String, required: true, },
   },
