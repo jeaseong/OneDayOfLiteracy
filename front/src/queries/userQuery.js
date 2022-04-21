de3dc907334fb44669f262405507d44064b9deac
handwoong <handwoong@gmail.com>
2022-04-21 23:28:53 +0900

    [FE] feat: 유저 로그인 상태 query 추가

--- front/src/queries/userQuery.js
+++ front/src/queries/userQuery.js
@@ -2,16 +2,19 @@ import { useQuery } from "react-query";
 import { get } from "../utils/api";
 
 export function useCurrentUser() {
+  let isLogin = false;
   const { isLoading, error, data, isFetching } = useQuery(
     "userState",
     () => get("user/current").then((res) => res.data),
     {
       refetchOnWindowFocus: false,
       retry: 0,
-      onSuccess: (data) => console.log(data),
+      onSuccess: (data) => {
+        if (!data) isLogin = true;
+      },
       onError: () => console.log("userToken 없음"),
     }
   );
 
-  return { userState: data, isLoading, error, isFetching };
+  return { userState: data, isLoading, isLogin, error, isFetching };
 }
