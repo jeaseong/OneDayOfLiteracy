0f1c088b6d1532acec17f13a87796ca2293065dc
YoungKon Kim <rladudrhs341@khu.ac.kr>
2022-04-21 11:21:55 +0900

    [BE] feat: user 스키마

---
+++ back/src/db/schemas/user.js
@@ -0,0 +1,14 @@
+import { Schema, model } from "mongoose";
+
+const UserSchema = new Schema({
+  nickname: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String },
+  badge: [{ type: String }],
+  level: { type: Number, default: 0 },
+  point: { type: Number, default: 0 },
+});
+
+const UserModel = model("User", UserSchema, "users");
+
+export { UserModel };
