2d439950110247040b31cafedadb43ac8b16ab10
guswn2521@gmail.com <guswn2521@gmail.com>
2022-04-22 00:20:59 +0900

    [FE][#13] feat:header nav tabs로 변경

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -1,18 +1,37 @@
 import React, { useContext, useState } from "react";
 // import { useQueryClient, useQuery } from "react-query";
-import { useNavigate, uselocation } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
+import { UserStateContext, DispatchContext } from "../App";
 
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Box from "@mui/material/Box";
 
+function LinkTab(props) {
+  return (
+    <Tab
+      component="a"
+      onClick={(event) => {
+        event.preventDefault();
+      }}
+      {...props}
+    />
+  );
+}
+
 function Header() {
   const [value, setValue] = React.useState("one");
-
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [show, setShow] = useState(false);
+  const userState = useContext(UserStateContext);
+  const dispatch = useContext(DispatchContext);
+
+  const isLogin = !!userState.user;
   // const queryClient = useQueryClient();
   // const query = useQuery();
 
@@ -32,9 +51,10 @@ function Header() {
             width="100"
           ></img>
 
-          <Tab value="one" label="서비스 소개" />
-          <Tab value="two" label="로그인" />
-          <Tab value="three" label="회원가입" />
+          <LinkTab href="/" label="서비스 소개" />
+          {isLogin && <LinkTab href="/logout" label="로그아웃" />}
+          <LinkTab href="/login" label="로그인" />
+          <LinkTab href="/register" label="회원가입" />
         </Tabs>
       </Box>
     </>
