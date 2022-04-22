3cff576aa05752c58455768111256dea88d816db
YoungKon Kim <rladudrhs341@khu.ac.kr>
2022-04-22 22:38:45 +0900

    [BE] fix: .env 에서 코드 공백, 줄바꿈 고침

--- back/index.js
+++ back/index.js
@@ -2,7 +2,8 @@ import config from "./src/config";
 import { app } from "./src/app";
 import mongoose from "mongoose";
 
-const PORT = config.port || 5000;
+const PORT = config.PORT || 5000;
+
 const DB_URL =
   config.mongodbUrl ||
   "MongoDB 서버 주소가 설정되지 않았습니다.\n./db/index.js 파일을 확인해 주세요.";
@@ -17,8 +18,6 @@ db.on("error", (error) =>
   console.error("MongoDB 연결에 실패하였습니다...\n" + DB_URL + "\n" + error)
 );
 
-
 app.listen(PORT, () => {
   console.log(`정상적으로 서버를 시작하였습니다.  http://localhost:${PORT}`);
 });
-
