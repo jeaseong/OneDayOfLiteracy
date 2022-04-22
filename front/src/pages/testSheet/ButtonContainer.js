393a8e89d52d66765804ea098da72d8fa7aba775
jjssong <wkdthdrnt@naver.com>
2022-04-23 03:04:18 +0900

    [FE][#23]feat: 마지막 test page에서 결과보기 버튼 출력

--- front/src/pages/testSheet/ButtonContainer.js
+++ front/src/pages/testSheet/ButtonContainer.js
@@ -2,11 +2,15 @@ import React, { useContext } from "react";
 import { testQuestion } from "./Utils";
 import { TestContext } from "../../context/TestContext";
 
-export const ButtonContainer = ({ nextTest, id }) => {
+export const ButtonContainer = ({ nextTest, id, onSubmit }) => {
   const { test, testDispatch } = useContext(TestContext);
   return (
     <div>
-      <button onClick={() => nextTest(id + 1)}>next</button>
+      {testQuestion.length + 1 !== id ? (
+        <button onClick={() => nextTest(id + 1)}>next</button>
+      ) : (
+        <button onClick={onSubmit}>결과보기</button>
+      )}
     </div>
   );
 };
