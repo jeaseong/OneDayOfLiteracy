94e62dccc639aee55a6f3ec3948ee997c46f6234
jjssong <wkdthdrnt@naver.com>
2022-04-24 01:47:45 +0900

    [FE][#36]refactor: useQuery를 이용한 get 요청 캐싱

---
+++ front/src/queries/TestQuery.js
@@ -0,0 +1,19 @@
+import { useMutation, useQuery, useQueryClient } from "react-query";
+import { get, post } from "../utils/api";
+
+export function useTestSheet() {
+  const queryClient = useQueryClient();
+  const { isLoading, error, data } = useQuery(
+    "test",
+    () => get("test").then((res) => res.data),
+    {
+      staleTime: Infinity,
+      onSuccess: () => console.log("test가 준비되었습니다."),
+      onError: () => {
+        console.log("테스트가 준비가 안됐습니다.");
+        queryClient.setQueryData("test", false);
+      },
+    }
+  );
+  return { test: data, isLoading, isTesting: data, error };
+}
