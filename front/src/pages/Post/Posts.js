2d4bf2edd6360bd5ac877b2b4d880d5fff57ed16
guswn2521@gmail.com <guswn2521@gmail.com>
2022-04-27 02:16:55 +0900

    [FE] 글 1개보기 navigate 호출

--- front/src/pages/Post/Posts.js
+++ front/src/pages/Post/Posts.js
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { get } from "../../utils/api";
-import { useCurrentUser } from "../../queries/userQuery";
 import PostCard from "./PostCard";
 import { PostContainer } from "../../styles/PostStyle";
 
 function Posts() {
-  // const user = useCurrentUser();
-  // console.log("--------------------", user);
   const [posts, setPosts] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const getPosts = async () => {
@@ -35,6 +34,9 @@ function Posts() {
                 ? post.imageUrls[0]
                 : "https://images.unsplash.com/photo-1532362996300-fbce5a30bd6d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80"
             }
+            onClick={() => {
+              navigate("posts/:postId");
+            }}
           />
         );
       })}
