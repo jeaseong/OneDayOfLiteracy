0b60f36de4532f560b4da3bdc5a2708000167e96
Hye-S <gptjs7970@naver.com>
2022-04-26 18:13:41 +0900

    [BE][#59] feat: validator middleware 추가

--- back/src/routers/userRouter.js
+++ back/src/routers/userRouter.js
@@ -1,6 +1,6 @@
-import is from "@sindresorhus/is";
 import { Router } from "express";
 import { loginRequired } from "../middlewares/loginRequired";
+import { isValidData, invalidCallback } from "../middlewares/validationMiddleware";
 import { userAuthService } from "../services/userService";
 import config from "../config";
 import axios from "axios";
@@ -8,50 +8,54 @@ import axios from "axios";
 const userAuthRouter = Router();
 
 // POST /user/register : user 추가
-userAuthRouter.post("/user/register", async (req, res, next) => {
-  try {
-    if (is.emptyObject(req.body)) {
-      throw new Error("잘못된 요청입니다.");
+userAuthRouter
+  .post("/user/register", 
+  isValidData("register"), 
+  invalidCallback, 
+  async (req, res, next) => {
+    try {
+      // req (request) 에서 데이터 가져오기
+      const email = req.body.email;
+      const password = req.body.password;
+      const nickname = req.body.nickname;
+
+      // 위 데이터를 유저 db에 추가하기
+      const newUser = await userAuthService.addUser({
+        email,
+        password,
+        nickname,
+      });
+
+      if (newUser.errorMessage) {
+        throw new Error(newUser.errorMessage);
+      }
+
+      res.status(201).json(newUser);
+    } catch (error) {
+      next(error);
     }
-
-    // req (request) 에서 데이터 가져오기
-    const email = req.body.email;
-    const password = req.body.password;
-    const nickname = req.body.nickname;
-
-    // 위 데이터를 유저 db에 추가하기
-    const newUser = await userAuthService.addUser({
-      email,
-      password,
-      nickname,
-    });
-
-    if (newUser.errorMessage) {
-      throw new Error(newUser.errorMessage);
-    }
-
-    res.status(201).json(newUser);
-  } catch (error) {
-    next(error);
-  }
 });
 
 // POST /user/login : user 로그인
-userAuthRouter.post("/user/login", async (req, res, next) => {
-  try {
-    const email = req.body.email;
-    const password = req.body.password;
+userAuthRouter
+  .post("/user/login",
+  isValidData("login"), 
+  invalidCallback, 
+  async (req, res, next) => {
+    try {
+      const email = req.body.email;
+      const password = req.body.password;
 
-    const user = await userAuthService.getUser({ email, password });
+      const user = await userAuthService.getUser({ email, password });
 
-    if (user.errorMessage) {
-      throw new Error(user.errorMessage);
+      if (user.errorMessage) {
+        throw new Error(user.errorMessage);
+      }
+
+      res.status(200).json(user);
+    } catch (error) {
+      next(error);
     }
-
-    res.status(200).json(user);
-  } catch (error) {
-    next(error);
-  }
 });
 
 // GET /oauth/kakao : kakaoUser 로그인
@@ -166,42 +170,46 @@ userAuthRouter.get("/user/current", loginRequired, async (req, res, next) => {
 });
 
 // PUT /user/:userId : user 정보 수정
-userAuthRouter.put("/user/:userId", loginRequired, async (req, res, next) => {
-  try {
-    const userType = req.currentUserType;
-    const currentUserId = req.currentUserId;
-    const { userId } = req.params;
-    const { nickname, password } = req.body;
-    
-    if(userId !== currentUserId) {
-      throw new Error('path parameter로 보낸 userId와 로그인한 userId가 달라서 수정을 제한합니다.');
-    }
+userAuthRouter.put("/user/:userId", 
+  loginRequired, 
+  isValidData("setting"),
+  invalidCallback,
+  async (req, res, next) => {
+    try {
+      const userType = req.currentUserType;
+      const currentUserId = req.currentUserId;
+      const { userId } = req.params;
+      const { nickname, password } = req.body;
+      
+      if(userId !== currentUserId) {
+        throw new Error('path parameter로 보낸 userId와 로그인한 userId가 달라서 수정을 제한합니다.');
+      }
 
-    let updatedUser = null;
-    if (userType === "general") {
-      const toUpdate = {
-        nickname,
-        password,
-      };
-      updatedUser = await userAuthService.setUser({ userId, toUpdate });
-    } else if (userType === "kakao") {
-      const toUpdate = {
-        nickname,
-      };
-      updatedUser = await userAuthService.setKakaoUser({ userId, toUpdate });
-    }
+      let updatedUser = null;
+      if (userType === "general") {
+        const toUpdate = {
+          nickname,
+          password,
+        };
+        updatedUser = await userAuthService.setUser({ userId, toUpdate });
+      } else if (userType === "kakao") {
+        const toUpdate = {
+          nickname,
+        };
+        updatedUser = await userAuthService.setKakaoUser({ userId, toUpdate });
+      }
 
-    if (updatedUser?.errorMessage) {
-      throw new Error(updatedUser.errorMessage);
-    } else if (updatedUser === null) {
-      //그럴 일은 없지만, 혹시 currentUserType이 잘못 저장되어 찾기 불가능한 상태
-      throw new Error("userType이 잘못되었습니다.");
-    }
+      if (updatedUser?.errorMessage) {
+        throw new Error(updatedUser.errorMessage);
+      } else if (updatedUser === null) {
+        //그럴 일은 없지만, 혹시 currentUserType이 잘못 저장되어 찾기 불가능한 상태
+        throw new Error("userType이 잘못되었습니다.");
+      }
 
-    res.status(200).json(updatedUser);
-  } catch (error) {
-    next(error);
-  }
+      res.status(200).json(updatedUser);
+    } catch (error) {
+      next(error);
+    }
 });
 
 // GET /users/:userId : 일반 user 조회
