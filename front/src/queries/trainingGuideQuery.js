cdca4371ad9f3658c07f9e6083011af437074f6b
jjssong <wkdthdrnt@naver.com>
2022-04-27 01:17:17 +0900

    [FE][#61]feat: 트레이닝 가이드 캐싱을 위한 react-query 훅 작성

---
+++ front/src/queries/trainingGuideQuery.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { useQuery } from "react-query";
+import { get } from "../utils/api";
+
+export function useTrainingGuide() {
+  const { isLoading, error, data } = useQuery(
+    "trainingstep",
+    () => get("training/guides").then((res) => res.data),
+    {
+      initialData: false,
+      staleTime: Infinity,
+      onSuccess: () => console.log("스켈레톤이 있으면 좋겠다."),
+      onError: () => console.log("실패할 일이 있을까.."),
+    }
+  );
+
+  return { userState: data, isLoading, isLogin: !!data, error };
+}
