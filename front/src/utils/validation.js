d81127ef428d92e53f0cf216b6ccc478ef232bbe
handwoong <handwoong@gmail.com>
2022-04-27 08:45:14 +0900

    [FE][#57] feat: 마이페이지 edit data 유효성 검사 구현

--- front/src/utils/validation.js
+++ front/src/utils/validation.js
@@ -13,11 +13,11 @@ function emailValidate(email) {
  * 비밀번호 형식을 검사합니다. (영문+숫자+8자리이상)
  * @param {string} type 로그인인지 회원가입인지 구분하기 위한 type입니다.
  * @param {string|object} passwordInfo type이 register가 아니라면 string입니다.
- * @returns {object} 비밀번호 유효성 검사 결과를 반환합니다.
+ * @returns {{ isPassRule: boolean, isSamePassword: boolean }} 비밀번호 유효성 검사 결과를 반환합니다.
  */
 function passwordValidate(type, passwordInfo) {
   const passwordRule = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$/;
-  if (type !== "register") return passwordRule.test(passwordInfo);
+  if (type === "login") return passwordRule.test(passwordInfo);
 
   const { password, confirmPassword } = passwordInfo;
   const isSamePassword =
@@ -31,7 +31,7 @@ function passwordValidate(type, passwordInfo) {
  * 회원가입 시 입력한 정보를 각 유효성 검사 함수로 전달합니다.
  * @param {string} type 로그인인지 회원가입인지 구분하기 위한 type입니다.
  * @param {object} info 유효성 검사를 진행 할 formData입니다.
- * @returns {object} 회원가입 formData 유효성 검사 결과를 반환합니다.
+ * @returns {{isCheckEmail: boolean,isSamePassword: boolean, isPassRule: boolean, isCheckNickName: boolean}} 회원가입 formData 유효성 검사 결과를 반환합니다.
  */
 function registerValidation(type, info) {
   const { email, password, confirmPassword, nickname } = info;
@@ -63,6 +63,24 @@ function loginValidation(type, info) {
 }
 
 /**
+ *
+ * @param {string} type
+ * @param {object} info
+ * @returns {{isSamePassword: boolean, isPassRule: boolean, isCheckNickName: boolean}}
+ */
+function editUserValidation(type, info) {
+  const { nickname, password, confirmPassword } = info;
+
+  const isCheckNickName = !(nickname.length > 2) && nickname.length > 0;
+  const { isPassRule, isSamePassword } = passwordValidate(type, {
+    password,
+    confirmPassword,
+  });
+
+  return { isCheckNickName, isPassRule, isSamePassword };
+}
+
+/**
  * 로그인 및 회원가입 시 type에 따라 formData 유효성 검사를 진행합니다.
  * @param {string} type 로그인인지 회원가입인지 구분하기 위한 type입니다.
  * @param {object} info 유효성 검사를 진행 할 formData입니다.
@@ -70,5 +88,6 @@ function loginValidation(type, info) {
  */
 export const validation = (type, info) => {
   if (type === "register") return registerValidation(type, info);
-  return loginValidation(type, info);
+  if (type === "login") return loginValidation(type, info);
+  if (type === "editUser") return editUserValidation(type, info);
 };
