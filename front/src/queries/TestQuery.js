130e02dab06e84a4101bfb4a4e6f5f958c0d1fd0
jjssong <wkdthdrnt@naver.com>
2022-04-28 04:02:25 +0900

    [FE][#36]refactor: query key 변경 및 isFetching으로 변경

--- front/src/queries/TestQuery.js
+++ front/src/queries/TestQuery.js
@@ -1,19 +1,19 @@
-import { useMutation, useQuery, useQueryClient } from "react-query";
-import { get, post } from "../utils/api";
+import { useQuery, useQueryClient } from "react-query";
+import { get } from "../utils/api";
 
 export function useTestQuery() {
   const queryClient = useQueryClient();
-  const { isLoading, error, data } = useQuery(
-    "test",
-    () => get("test").then((res) => res.data),
+  const { isFetching, error, data } = useQuery(
+    "tests",
+    () => get("tests").then((res) => res.data),
     {
       staleTime: Infinity,
       onSuccess: () => console.log("test가 준비되었습니다."),
       onError: () => {
         console.log("테스트가 준비가 안됐습니다.");
-        queryClient.setQueryData("test", false);
+        queryClient.setQueryData("tests", false);
       },
     }
   );
-  return { test: data, isLoading, isTesting: data, error };
+  return { tests: data, isFetching, error };
 }
