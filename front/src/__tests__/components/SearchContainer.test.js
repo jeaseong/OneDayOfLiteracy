87fab12a6e9cbe167650c6216e8b4c0a16e15494
handwoong <handwoong@gmail.com>
2022-04-28 23:15:11 +0900

    [FE][#73] test: 검색창 유저입력 테스트 코드 추가

--- front/src/__tests__/components/SearchContainer.test.js
+++ front/src/__tests__/components/SearchContainer.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from "../../test-utils";
 import SearchContainer from "../../components/Search/SearchContainer";
+import userEvent from "@testing-library/user-event";
 
 test("검색창과 카테고리 선택 요소가 있다.", () => {
   render(<SearchContainer />);
@@ -11,4 +12,17 @@ test("검색창과 카테고리 선택 요소가 있다.", () => {
 
   const searchButton = screen.getByRole("button", { name: "검색" });
   expect(searchButton).toBeInTheDocument();
+});
+
+test("검색창과 카테고리 선택 시 동작 테스트", () => {
+  render(<SearchContainer />);
+  const searchInput = screen.getByPlaceholderText("검색하기");
+  userEvent.type(searchInput, "테스트검색");
+  expect(searchInput).toHaveValue("테스트검색");
+
+  userEvent.selectOptions(
+    screen.getByRole("combobox"),
+    screen.getByRole("option", { name: "에세이" })
+  );
+  expect(screen.getByRole("option", { name: "에세이" })).toBeInTheDocument();
 });
