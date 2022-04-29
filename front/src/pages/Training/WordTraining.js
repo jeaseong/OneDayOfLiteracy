a1f81a885fd56ec59140a3ef877683b5c19d81d3
jjssong <wkdthdrnt@naver.com>
2022-04-29 19:50:36 +0900

    [FE][#61]feat: 단어공부 기능, 프로그레스바 추가

--- front/src/pages/Training/WordTraining.js
+++ front/src/pages/Training/WordTraining.js
@@ -5,32 +5,60 @@ import {
   WordMeaning,
   ProgressBox,
   ProgressBar,
+  Progress,
+  ProgressPercent,
+  WordMeaningBox,
   ShowAnswerBtn,
   ButtonBox,
   PrevBtn,
   NextBtn,
+  Hilight,
 } from "../../styles/WordTrainingStyle";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import { dum } from "./wordDumy";
+import "../../index.css";
 
 export default function WordTraining() {
   const [progress, setProgress] = useState(0);
   const [hopeAnswer, setHopeAnswer] = useState(false);
+
+  const handleClick = (direction) => {
+    setProgress((cur) => cur + direction);
+    setHopeAnswer(false);
+  };
   return (
     <WordTrainingContainer>
       <WordSuggestion>제시어: {dum[progress].word}</WordSuggestion>
-      <WordMeaning>{dum[progress].meaning}</WordMeaning>
-      <ShowAnswerBtn>정답보기</ShowAnswerBtn>
+      <WordMeaningBox>
+        <WordMeaning>{hopeAnswer && dum[progress].meaning}</WordMeaning>
+      </WordMeaningBox>
+      <ShowAnswerBtn
+        onClick={() => {
+          setHopeAnswer((cur) => !cur);
+        }}
+      >
+        정답보기
+      </ShowAnswerBtn>
       <ProgressBox>
-        <ProgressBar />
+        <ProgressBar>
+          <Progress
+            width={`${Math.ceil(((progress + 1) / dum.length) * 100)}%`}
+          />
+        </ProgressBar>
+        <ProgressPercent>
+          {Math.ceil(((progress + 1) / dum.length) * 100)}%
+        </ProgressPercent>
       </ProgressBox>
       <ButtonBox>
-        <PrevBtn>
-          <ArrowBackIosIcon />
+        <PrevBtn disabled={progress === 0} onClick={() => handleClick(-1)}>
+          <ArrowBackIosIcon fontSize="large" />
         </PrevBtn>
-        <NextBtn>
-          <ArrowForwardIosIcon />
+        <NextBtn
+          disabled={progress === dum.length - 1}
+          onClick={() => handleClick(1)}
+        >
+          <ArrowForwardIosIcon fontSize="large" />
         </NextBtn>
       </ButtonBox>
     </WordTrainingContainer>
