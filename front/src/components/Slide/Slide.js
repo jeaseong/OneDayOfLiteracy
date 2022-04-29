1acbba9e6cff57b98e4b326384e87b11ca9e09e4
jjssong <wkdthdrnt@naver.com>
2022-04-29 12:31:53 +0900

    [FE][#61]feat: 스와이프 슬라이더 바 구현

--- front/src/components/Slide/Slide.js
+++ front/src/components/Slide/Slide.js
@@ -15,6 +15,9 @@ const initTransition = "transform 0.5s";
 export default function Slide({ elements }) {
   const [curIndex, setCurIndex] = useState(0);
   const [curTransition, setCurTransition] = useState(initTransition);
+  const [isSwiping, setIsSwiping] = useState(false);
+  const [slideX, setSlideX] = useState(null);
+  const [prevSlideX, setPrevSlideX] = useState(false);
   const ORIGINSIZE = elements.length;
   const infiniteElements = [elements[ORIGINSIZE - 1], ...elements, elements[0]];
   const NEWSIZE = infiniteElements.length;
@@ -38,6 +41,42 @@ export default function Slide({ elements }) {
     setCurTransition(initTransition);
   };
 
+  const handleSwipe = (direction) => {
+    setIsSwiping(true);
+    handleSlide(curIndex + direction);
+  };
+
+  const getClientX = (event) => {
+    return event._reactName === "onTouchStart"
+      ? event.touches[0].clientX
+      : event._reactName === "onTouchMove" || event._reactName === "onTouchEnd"
+      ? event.changedTouches[0].clientX
+      : event.clientX;
+  };
+
+  const handleTouchStart = (e) => {
+    setPrevSlideX((prevSlideX) => getClientX(e));
+  };
+
+  const handleTouchMove = (e) => {
+    if (prevSlideX) {
+      setSlideX((slideX) => getClientX(e) - prevSlideX);
+    }
+  };
+
+  const handleMouseSwipe = (e) => {
+    if (slideX) {
+      const currentTouchX = getClientX(e);
+      if (prevSlideX > currentTouchX + 100) {
+        handleSlide(curIndex + 1);
+      } else if (prevSlideX < currentTouchX - 100) {
+        handleSlide(curIndex - 1);
+      }
+      setSlideX((slideX) => null);
+    }
+    setPrevSlideX((prevSlideX) => null);
+  };
+
   return (
     <>
       <OverFlow>
@@ -45,9 +84,20 @@ export default function Slide({ elements }) {
           width={`${NEWSIZE * 100}vw`}
           transform={`translate(${-100 * curIndex}vw)`}
           transition={curTransition}
+          onMouseOver={() => setIsSwiping(true)}
+          onMounseOut={() => setIsSwiping(false)}
         >
           {infiniteElements.map((e, index) => (
-            <SlideInner key={index}>
+            <SlideInner
+              key={index}
+              onMouseDown={handleTouchStart}
+              onTouchStart={handleTouchStart}
+              onTouchMove={handleTouchMove}
+              onMouseMove={handleTouchMove}
+              onMouseUp={handleMouseSwipe}
+              onTouchEnd={handleMouseSwipe}
+              onMouseLeave={handleMouseSwipe}
+            >
               <SlideItem>
                 <img src={e} alt="사진" />
               </SlideItem>
@@ -56,10 +106,10 @@ export default function Slide({ elements }) {
         </SlideContainer>
       </OverFlow>
       <BottonContainer>
-        <PrevBtn onClick={() => handleSlide(curIndex - 1)}>
+        <PrevBtn onClick={() => handleSwipe(-1)}>
           <ArrowBackIosIcon fontSize="large" />
         </PrevBtn>
-        <NextBtn onClick={() => handleSlide(curIndex + 1)}>
+        <NextBtn onClick={() => handleSwipe(1)}>
           <ArrowForwardIosIcon fontSize="large" />
         </NextBtn>
       </BottonContainer>
