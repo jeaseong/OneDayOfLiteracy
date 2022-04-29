87f0ff1acf108f87d132ea6d1a13e29240222e6d
jjssong <wkdthdrnt@naver.com>
2022-04-29 19:51:35 +0900

    [FE][#61]style: 버튼, 프로그레스바 강조 단어 스타일 추가

--- front/src/styles/WordTrainingStyle.js
+++ front/src/styles/WordTrainingStyle.js
@@ -7,38 +7,84 @@ export const WordTrainingContainer = styled.section`
   justify-content: center;
 `;
 
-export const WordSuggestion = styled.h3``;
+export const WordSuggestion = styled.h3`
+  font-size: 24px;
+  margin-bottom: 10px;
+`;
 
-export const WordMeaning = styled.p``;
+export const WordMeaningBox = styled.div`
+  min-width: 300px;
+  min-height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid #445656;
+  margin-bottom: 20px;
+`;
 
-export const ProgressBox = styled.div``;
-
-export const ProgressBar = styled.div``;
+export const WordMeaning = styled.p`
+  font-size: 18px;
+`;
 
 export const ShowAnswerBtn = styled.button`
   display: flex;
   justify-content: center;
   align-items: center;
-  border: 2px solid #445656;
-  border-radius: 30px;
+  text-align: center;
   color: #445656;
   background-color: none;
+  border: 2px solid #445656;
+  border-radius: 30px;
   padding: 6px 12px;
-  text-align: center;
+  margin-bottom: 20px;
+`;
+
+export const ProgressBox = styled.div`
+  display: flex;
+  margin-bottom: 20px;
+`;
+
+export const ProgressBar = styled.div`
+  width: 200px;
+  margin-right: 6px;
+  border: 1px solid #c48f5a;
+  border-radius: 10px;
+  display: flex;
+  align-items: center;
+`;
+
+export const Progress = styled.div`
+  width: ${(props) => props.width};
+  height: 98%;
+  border-radius: 10px;
+  background-color: #c48f5a;
+  transition: width 0.2s;
+`;
+
+export const ProgressPercent = styled.p`
+  color: #c48f5a;
 `;
 
 export const ButtonBox = styled.div`
-  width: 100%;
+  min-width: 100px;
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 export const Btn = styled.button`
   border: none;
   background: none;
   color: #c48f5a;
-  font-size: 20px;
+  text-align: center;
+  &:disabled {
+    color: #c4c4c4;
+  }
 `;
 
 export const PrevBtn = styled(Btn)``;
 export const NextBtn = styled(Btn)``;
+
+export const Hilight = styled.p`
+  color: #c48f5a;
+`;
