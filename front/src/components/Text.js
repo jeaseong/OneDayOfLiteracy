9e1523ea069c63b8e16d251009df81cc78acf392
jjssong <wkdthdrnt@naver.com>
2022-04-29 22:40:12 +0900

    [FE][]feat: text 컴포넌트로 줄바꿈 쉽게 하기

---
+++ front/src/components/Text.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { TextComponent } from "../styles/componentStyle";
+
+export default function Text({ children }) {
+  return <TextComponent>{children}</TextComponent>;
+}
