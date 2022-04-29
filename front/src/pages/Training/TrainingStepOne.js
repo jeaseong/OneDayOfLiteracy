57694ea2063e50538e0209810b6121328ee12c04
jjssong <wkdthdrnt@naver.com>
2022-04-30 01:23:52 +0900

    [FE][#61]rerfactor: Text 컴포넌트로 텍스트 줄바꿈 쉽게 처리 가능

--- front/src/pages/Training/TrainingStepOne.js
+++ front/src/pages/Training/TrainingStepOne.js
@@ -8,6 +8,7 @@ import {
   TrainingStepIntroduction,
   Hilight,
 } from "../../styles/TrainingStyle";
+import Text from "../../components/Text";
 function TrainingStepOne() {
   return (
     <TrainingGuide>
@@ -15,11 +16,14 @@ function TrainingStepOne() {
         <TrainingSubjectWrap>
           <TrainingStepTitle>1단계</TrainingStepTitle>
           <TrainingStepIntroduction>
-            가장 기본은 문장의 의미를 <Hilight>정확하게 아는 것</Hilight>이
-            중요합니다. <br />
-            간단한 단어 퀴즈를 통해 <Hilight>어휘력</Hilight>을 길러봅시다!
-            <br />
-            제시된 단어의 뜻을 생각해보세요.
+            <Text>
+              가장 기본은 문장의 의미를 <Hilight>정확하게 아는 것</Hilight>이
+              중요합니다.
+            </Text>
+            <Text>
+              간단한 단어 퀴즈를 통해 <Hilight>어휘력</Hilight>을 길러봅시다!
+            </Text>
+            <Text>제시된 단어의 뜻을 생각해보세요.</Text>
           </TrainingStepIntroduction>
         </TrainingSubjectWrap>
       </TrainingSubjectContainer>
