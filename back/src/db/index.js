774c556182b21a365f71833ca69b7c2a2888f124
Hye-S <gptjs7970@naver.com>
2022-04-29 15:03:54 +0900

    [BE] fix: db/index.js 에서 kakaoUser Model 삭제

--- back/src/db/index.js
+++ back/src/db/index.js
@@ -1,11 +1,10 @@
 // model export
 
 import { User } from "./models/User";
-import { KakaoUser } from "./models/KakaoUser";
 import { Post } from "./models/Post";
 import { Test } from "./models/Test";
 import { Result } from "./models/Result";
 import { Subject } from "./models/Subject";
 import { Like } from "./models/Like";
 
-export { User, KakaoUser, Post, Test, Result, Subject, Like };
+export { User, Post, Test, Result, Subject, Like };
