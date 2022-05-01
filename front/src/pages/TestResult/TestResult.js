1fa522533919e6a04ce34123714b964577553c8b
jjssong <wkdthdrnt@naver.com>
2022-05-01 09:44:53 +0900

    [FE][#23]feat: 테스트 결과 보기

--- front/src/pages/TestResult/TestResult.js
+++ front/src/pages/TestResult/TestResult.js
@@ -1,6 +1,64 @@
-import React from "react";
-import { TestResultContainer } from "../../styles/TestResult";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { get } from "../../utils/api";
+import {
+  TestResultContainer,
+  TestResultWrap,
+  TestResultUserName,
+  TestResultUserScore,
+  TestResultUserRecommand,
+  TestResultNavBtn,
+  Hilight,
+} from "../../styles/TestResultStyle";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import { useGetCurrentUser } from "../../queries/userQuery";
+import { recommend } from "../../utils/testResult";
 
 export default function TestResult() {
-  return <TestResultContainer></TestResultContainer>;
+  const navigate = useNavigate();
+  const [myScore, setMyScore] = useState(0);
+  const { userState } = useGetCurrentUser();
+
+  const recommendStep = (score) => {
+    if (score >= 90) return recommend[2];
+    else if (score >= 70) return recommend[1];
+    else return recommend[0];
+  };
+
+  const handleClickNavBtn = (score) => {
+    if (score >= 90) return 3;
+    else return 1;
+  };
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      const res = await get("results/", userState._id);
+      setMyScore(res.data[res.data.length - 1].result);
+    };
+    fetchAPI();
+  }, []);
+
+  return (
+    <TestResultContainer>
+      <TestResultWrap>
+        <TestResultUserName>
+          <Hilight>{userState.nickname} </Hilight>님의 점수는
+        </TestResultUserName>
+        <TestResultUserScore>
+          <Hilight>{myScore || "0"}/100</Hilight> 입니다!
+        </TestResultUserScore>
+        <TestResultUserRecommand>
+          {recommendStep(myScore)}
+        </TestResultUserRecommand>
+        <TestResultNavBtn
+          onClick={() => {
+            navigate(`/training/${handleClickNavBtn(myScore)}`);
+          }}
+        >
+          <>서비스 바로가기</>
+          <ArrowForwardIcon />
+        </TestResultNavBtn>
+      </TestResultWrap>
+    </TestResultContainer>
+  );
 }
---
+++ front/src/styles/TestResultStyle.js
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+import { FlexBoxCenter } from "./CommonStyle";
+
+export const TestResultContainer = styled(FlexBoxCenter)`
+  width: 100%;
+  height: 30vh;
+  flex-direction: column;
+  text-align: center;
+  line-height: 1.5;
+`;
+
+export const TestResultWrap = styled.div``;
+
+export const TestResultUserName = styled.h2`
+  text-align: left;
+  margin-bottom: 10px;
+`;
+
+export const TestResultUserScore = styled.p`
+  font-size: 18px;
+  font-weight: bold;
+  margin-bottom: 10px;
+`;
+
+export const TestResultUserRecommand = styled.p`
+  word-break: break-all;
+  white-space: pre-wrap;
+  font-size: 16px;
+  margin-bottom: 20px;
+`;
+
+export const TestResultNavBtn = styled.button`
+  border: none;
+  background: none;
+  display: flex;
+  align-items: center;
+  margin: 0 auto;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    color: #c48f5a;
+    transform: scale(1.1);
+  }
+`;
+
+export const Hilight = styled.mark`
+  font-weight: bold;
+  background: none;
+  color: #c48f5a;
+  font-size: 24px;
+`;
