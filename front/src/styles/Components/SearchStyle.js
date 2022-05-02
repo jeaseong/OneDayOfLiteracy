9b5c86367a2542610193aea8b346d0b35388e7a1
handwoong <handwoong@gmail.com>
2022-05-02 17:21:29 +0900

    [FE] style: 검색바 style 수정

--- front/src/styles/Components/SearchStyle.js
+++ front/src/styles/Components/SearchStyle.js
@@ -19,8 +19,8 @@ export const SearchButton = styled(Button)`
 export const SearchInput = styled.input`
   border: solid 2px #c99c6e;
   border-radius: 8px;
-  width: 95%;
-  height: 38px;
+  width: 90%;
+  height: 22px;
   font-size: 1.3rem;
   padding: 6px;
 `;
