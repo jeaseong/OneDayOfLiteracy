584213bb3c9316757909b57136e8e361c9fcad5e
Hye-S <gptjs7970@naver.com>
2022-05-02 22:48:55 +0900

    [BE][#103] feat: comment schema, model 작성

---
+++ back/src/db/models/Comment.js
@@ -0,0 +1,47 @@
+import { CommentModel } from '../schemas/comment';
+
+class Comment {
+  static async create({ newComment }) {
+    const createdComment = await CommentModel.create(newComment);
+    return createdComment;
+  }
+
+  static async findByPostId({ postId }) {
+    const comments = await CommentModel.find({ postId }).populate('childComments');
+    return comments;
+  }
+
+  static async findById({ commentId }) {
+    const comment = await CommentModel.findOne({ _id: commentId }).populate('childComments');
+    return comment;
+  }
+
+  static async update({ commentId, toUpdate }) {
+    const filter = { _id: commentId };
+    const option = { returnOriginal: false };
+    const updatedComment = await CommentModel.findOneAndUpdate(
+      filter,
+      toUpdate,
+      option
+    );
+
+    return updatedComment;
+  }
+
+  static async delete({ commentId }) {
+    // soft delete 방식 사용
+    const filter = { _id: commentId };
+    const toUpdate = { isDeleted: true };
+    const option = { returnOriginal: false };
+
+    const deletedComment = await CommentModel.findOneAndUpdate(
+      filter,
+      toUpdate,
+      option,
+    );
+
+    return deletedComment;
+  }
+}
+
+export { Comment };
