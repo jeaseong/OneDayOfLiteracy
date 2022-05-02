02a57ecc70531af11432f6041f1cd8856ab7c639
handwoong <handwoong@gmail.com>
2022-05-02 20:13:27 +0900

    [FE] fix: 검색 엔드포인트 수정

--- front/src/pages/Post/Posts.js
+++ front/src/pages/Post/Posts.js
@@ -10,7 +10,7 @@ import ErrorPage from "../../components/ErrorPage";
 function Posts() {
   const location = useLocation();
   const { ref, inView } = useInView();
-  const fetchURI = `posts?${location.search}&`;
+  const fetchURI = `posts?${location.search.substring(1)}&`;
   const { data, status, fetchNextPage, isFetchingNextPage } =
     useGetPostList(fetchURI);
 
