18eddd9754fed6e294ba07561406231642a66729
jjssong <wkdthdrnt@naver.com>
2022-05-05 04:33:52 +0900

    [FE][#117]feat: 무한 스크롤 댓글창 구현

--- front/src/pages/Comment/Comment.js
+++ front/src/pages/Comment/Comment.js
@@ -1,32 +1,43 @@
 import React, { useState, useEffect } from "react";
 import { useInView } from "react-intersection-observer";
+import { useGetCommentList } from "queries/commentQuery";
 import CommentInput from "pages/Comment/CommentInput";
 import CommentSingle from "pages/Comment/CommentSingle";
 import CommentReply from "pages/Comment/CommentReply";
-import { useGetCommentList } from "queries/commentQuery";
 import { CommentContainer, UserCommentList } from "styles/Comment/CommentStyle";
-export default function Comment() {
+import Loading from "components/Loading";
+import ErrorPage from "components/ErrorPage";
+export default function Comment({ postId }) {
   const { data, status, fetchNextPage, isFetchingNextPage } =
-    useGetCommentList();
+    useGetCommentList(postId);
+  const { ref, inView } = useInView();
+  useEffect(() => {
+    if (inView) fetchNextPage();
+  }, [inView]);
+
+  if (status === "loading") return <Loading />;
+  if (status === "error") return <ErrorPage />;
 
   return (
-    <CommentContainer>
-      <CommentInput />
-      <UserCommentList>
-        <CommentSingle />
-        <CommentReply />
-        {/* {data?.pages.map((page, i) => (
-          <React.Fragment key={i}>
-            {page.comments.map((comment, k) => (
-              <>
-                <CommentSingle comment={comment} />
-                {comment.reply.length > 0 && <CommentReply reComment={comment.reply} />}
-                
-              </>
-            ))}
-          </React.Fragment>
-        ))} */}
-      </UserCommentList>
-    </CommentContainer>
+    <>
+      <CommentContainer>
+        <CommentInput />
+        <UserCommentList>
+          {data?.pages?.map((page, i) => (
+            <React.Fragment key={i}>
+              {page.comments?.map((comment, k) => (
+                <>
+                  <CommentSingle comment={comment} />
+                  {comment.childComments.length > 0 && (
+                    <CommentReply reComment={comment.reply} />
+                  )}
+                </>
+              ))}
+            </React.Fragment>
+          ))}
+        </UserCommentList>
+      </CommentContainer>
+      {isFetchingNextPage ? <Loading /> : <div ref={ref}></div>}
+    </>
   );
 }
