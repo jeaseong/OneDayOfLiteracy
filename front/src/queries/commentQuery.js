9f7669559bf6153cd6e69014618eeade5f9b52f1
jjssong <wkdthdrnt@naver.com>
2022-05-05 04:37:07 +0900

    [FE][#117]feat: 수정, 삭제에 대한 커스텀 훅

--- front/src/queries/commentQuery.js
+++ front/src/queries/commentQuery.js
@@ -1,9 +1,9 @@
-import { useInfiniteQuery } from "react-query";
-import { get } from "utils/api";
+import { useInfiniteQuery, useQueryClient, useMutation } from "react-query";
+import { get, post, put, del } from "utils/api";
 
-export function useGetCommentList(postId) {
+export const useGetCommentList = (postId) => {
   const fetchCommentList = async ({ pageParam = 1 }) => {
-    const res = await get("");
+    const res = await get(`posts/${postId}/comments?page=${pageParam}&limit=6`);
     const { comments, isLast } = res.data;
     return { comments, nextPage: pageParam + 1, isLast };
   };
@@ -14,4 +14,48 @@ export function useGetCommentList(postId) {
     getNextPageParam: (lastPage) =>
       !lastPage.isLast ? lastPage.nextPage : undefined,
   });
-}
+};
+
+export const usePostComment = (postId) => {
+  const queryClient = useQueryClient();
+  //comment에 parentId가 있으면 대댓글, 없으면 일반 부모 댓글
+  // parentId 있으면 setQueryData 해줘야됨. 바로 보이게
+  return useMutation(
+    async (comment) => await post(`posts/${comment.postId}/comments`, comment),
+    {
+      onMutate: (comment) => {
+        const curComments = queryClient.getQueryData([
+          "comments",
+          comment.postId,
+        ]);
+      },
+      onSuccess: () => queryClient.invalidateQueries(["comments", postId]),
+
+      onError: (err) => console.log(err),
+    }
+  );
+};
+
+export const useUpdateComment = (commentId, content, postId) => {
+  const queryClient = useQueryClient();
+  return useMutation(
+    (commentId, content) => put(`comments/${commentId}`, content),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(["comments", postId]);
+        console.log("성공?");
+      },
+      onError: () => console.log("이게 모얌"),
+    }
+  );
+};
+
+export const useDeleteComment = () => {
+  const queryClient = useQueryClient();
+  return useMutation((comment) => del(`comments/`, comment._id), {
+    onSuccess: () => {
+      queryClient.invalidateQueries("comments");
+    },
+    onError: () => console.log("이게 모얌"),
+  });
+};
