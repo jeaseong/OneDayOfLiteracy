0f8cfd62e6af52a932777d2beec493436a20831f
handwoong <handwoong@gmail.com>
2022-05-04 20:06:58 +0900

    [FE][#116] chore: 유저 엔드포인트 수정

--- front/src/queries/userQuery.js
+++ front/src/queries/userQuery.js
@@ -12,7 +12,7 @@ export function useGetCurrentUser() {
   return useQuery(
     "userState",
     async () => {
-      const res = await get("user/current");
+      const res = await get("users/current");
       return { userState: res.data, isLogin: !!res.data };
     },
     {
@@ -73,15 +73,18 @@ export const useUserLoginHandler = (setShowAlert = () => {}) => {
   const navigate = useNavigate();
   const queryClient = useQueryClient();
 
-  return useMutation(async (loginData) => await post("user/login", loginData), {
-    onSuccess: (res) => {
-      const jwtToken = res.data.token;
-      localStorage.setItem("userToken", jwtToken);
-      queryClient.invalidateQueries("userState");
-      navigate("/");
-    },
-    onError: () => setShowAlert(true),
-  });
+  return useMutation(
+    async (loginData) => await post("users/login", loginData),
+    {
+      onSuccess: (res) => {
+        const jwtToken = res.data.token;
+        localStorage.setItem("userToken", jwtToken);
+        queryClient.invalidateQueries("userState");
+        navigate("/");
+      },
+      onError: () => setShowAlert(true),
+    }
+  );
 };
 
 /**
