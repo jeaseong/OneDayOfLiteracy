6b2440fa53af5c938ba5887dc0cb48dfb16408cb
jjssong <wkdthdrnt@naver.com>
2022-05-05 04:35:43 +0900

    [FE][#117]feat: 내가 쓴 글만 수정, 삭제가 가능

--- front/src/pages/Comment/CommentSingle.js
+++ front/src/pages/Comment/CommentSingle.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
-import CommentInput from "./CommentInput";
+import CommentInput from "pages/Comment/CommentInput";
+import CommentEditFrom from "pages/Comment/CommentEditForm";
+import { useQueryClient } from "react-query";
+import { useDeleteComment } from "queries/commentQuery";
 import {
   CommentBox,
   UserThumbnail,
@@ -8,13 +11,22 @@ import {
   UserName,
   Comment,
   ReplyCommentBtn,
+  CommentEditContainer,
+  CommentEditBtn,
 } from "styles/Comment/CommentStyle";
 
 export default function CommentSingle({ comment }) {
   const [isOpenReply, setIsOpenReply] = useState(false);
+  const [isEdit, setIsEdit] = useState(false);
+  const queryClient = useQueryClient();
+  const { userState } = queryClient.getQueryData("userState");
+  const deleteComment = useDeleteComment();
 
   const onClickOpenReplyInput = () => {
     setIsOpenReply((cur) => !cur);
+  };
+  const deleteSubmitComment = () => {
+    deleteComment.mutate(comment);
   };
   return (
     <CommentBox>
@@ -22,9 +34,34 @@ export default function CommentSingle({ comment }) {
         <Profile />
       </UserThumbnail>
       <CommentContent>
-        <UserName>박재성</UserName>
-        <Comment>이거 댓글이 너무 멋집니다!</Comment>
-        <ReplyCommentBtn onClick={onClickOpenReplyInput}>답글</ReplyCommentBtn>
+        {!isEdit ? (
+          <>
+            <UserName>{comment.author}</UserName>
+            <Comment>{comment.content}</Comment>
+          </>
+        ) : (
+          <CommentEditFrom
+            prev={comment.content}
+            commentId={comment._id}
+            postId={comment.postId}
+          />
+        )}
+
+        <CommentEditContainer>
+          <ReplyCommentBtn onClick={onClickOpenReplyInput}>
+            답글
+          </ReplyCommentBtn>
+          {userState.id === comment.userId && (
+            <>
+              <CommentEditBtn onClick={() => setIsEdit((cur) => !cur)}>
+                {isEdit ? "취소" : "수정"}
+              </CommentEditBtn>
+              <CommentEditBtn onClick={deleteSubmitComment}>
+                삭제
+              </CommentEditBtn>
+            </>
+          )}
+        </CommentEditContainer>
         {isOpenReply && <CommentInput parentId={1} />}
       </CommentContent>
     </CommentBox>
