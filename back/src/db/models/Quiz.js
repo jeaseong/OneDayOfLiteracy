848249ae2c94e64ebe8c61c3c2c7e851d17f4116
YoungKon Kim <rladudrhs341@khu.ac.kr>
2022-05-06 13:08:17 +0900

    [BE] feat: UserWord 조회 시 num, word 가 있는 객체 반환하도록 수정

--- back/src/db/models/Quiz.js
+++ back/src/db/models/Quiz.js
@@ -7,17 +7,17 @@ class Quiz{
     }
     
     static async findByNum({ num }){
-        const quiz = await QuizModel.findOne({ num }, {__v:0});
+        const quiz = await QuizModel.findOne({ num }, {__v:0}).lean();
         return quiz;
     }
     
     static async findByWord({ word }){
-        const quiz = await QuizModel.findOne({ word }, {__v: 0});
+        const quiz = await QuizModel.findOne({ word }, {__v: 0}).lean();
         return quiz;
     }
 
     static async findAll(){
-        const quizzes = await QuizModel.find({},{ num: 1, word: 1, meaning: 1});
+        const quizzes = await QuizModel.find({},{ num: 1, word: 1, meaning: 1}).lean();
         return quizzes;
     }
 }
