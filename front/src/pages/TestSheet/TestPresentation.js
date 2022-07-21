cedd1330dd11e2065a6d5b2e84c900c20e598523
jsPark <wotjddnfl@gmail.com>
2022-07-22 00:55:16 +0900

    [FE]refactor: HOC로 파일 단순화

--- front/src/pages/TestSheet/TestPresentation.js
+++ front/src/pages/TestSheet/TestPresentation.js
@@ -1,6 +1,13 @@
 import React from "react";
 import TestAnswerBtn from "pages/TestSheet/TestAnswerBtn";
-import { TestContent, TestQuestion } from "styles/Test/TestStyle";
+import { 
+  TestContent,
+  TestQuestion,
+  AnswerBtn,
+  AnswerText,
+  AnswerBtnContainer,
+} from "styles/Test/TestStyle";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 import { createMarkup } from "utils/setInnerHTML";
 
 export default function TestPresentation({
@@ -8,20 +15,39 @@ export default function TestPresentation({
   selectedAnswer,
   MyselectedAnswer,
 }) {
+  const {num, choices} = test
   return (
     <>
       <TestQuestion>
-        Q{test.num}. {test.question}
+        Q{num}. {test.question}
       </TestQuestion>
       <TestContent
         dangerouslySetInnerHTML={createMarkup(test.content)}
       ></TestContent>
-      <TestAnswerBtn
+      <AnswerBtnContainer>
+      {choices &&
+        Object.entries(choices).map((option, index) => (
+          <AnswerBtn
+            key={index}
+            onClick={() => {
+              MyselectedAnswer(num, option[0]);
+            }}
+          >
+            <AnswerText>
+              {index + 1}. {option[1]}
+            </AnswerText>
+            {selectedAnswer === option[0] && (
+              <FavoriteIcon style={{ color: "#C48F5A" }} />
+            )}
+          </AnswerBtn>
+        ))}
+    </AnswerBtnContainer>
+      {/* <TestAnswerBtn
         testId={test.num}
         choices={test.choices}
         selectedAnswer={selectedAnswer}
         MyselectedAnswer={MyselectedAnswer}
-      />
+      /> */}
     </>
   );
 }
